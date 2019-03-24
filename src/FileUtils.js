e051d0abcf3dace1ac048fb521bf618c7aeae7cf
Frank <frankyoh@tmon.co.kr>
2019-03-24 10:03:36 +0900

    fixed copying sound files issue

--- src/FileUtils.js
+++ src/FileUtils.js
@@ -1,20 +1,26 @@
 import fs from 'fs'
 import constants from './constants'
+import { getInstalledPathSync } from 'get-installed-path'
+import path from 'path'
 
 export default class FileUtils {
   static copyFilesInSrcDirToDstDir (srcDir, dstDir) {
-    console.log('copying ', srcDir, 'to', dstDir)
+    let rootPath = fs.existsSync(path.join(getInstalledPathSync(constants.programName), srcDir)) ? getInstalledPathSync(constants.programName) : ''
+    srcDir = path.join(rootPath, srcDir)
+    // console.info('srcDir', srcDir)
 
     if (!fs.existsSync(constants.userHomeLibrarySoundPath)) {
       fs.mkdirSync(constants.userHomeLibrarySoundPath)
     }
 
-    fs.readdirSync(srcDir).forEach(file => {
-      let srcFile = srcDir + '/' + file
-      fs.copyFile(srcFile, constants.userHomeLibrarySoundPath + '/' + file, (err => {
-        if (err) throw err
-        console.info('copying ', srcFile, 'sound file to', constants.userHomeLibrarySoundPath)
-      }))
-    })
+    if (fs.existsSync(srcDir)) {
+      fs.readdirSync(srcDir).forEach(file => {
+        let srcFile = srcDir + '/' + file
+        fs.copyFile(srcFile, dstDir + '/' + file, (err => {
+          if (err) throw err
+          // console.info('copying ', srcFile, 'sound file to', dstDir)
+        }))
+      })
+    }
   }
 }
